835fa7a2d6b231231a27b392ed0774d9713a8868
40343450 <54434644+40343450@users.noreply.github.com>
2021-03-09 23:50:20 +0000

    lab06

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -33,10 +33,92 @@ public class Employee
     /**
      * Employee's current department
      */
-    public String dept_name;
+    public Department dept_name;
 
     /**
      * Employee's manager
      */
-    public String manager;
+    public Employee manager;
+
+    public Employee() {
+    }
+
+    public Employee(int emp_no, String first_name, String last_name, String title, int salary, Department dept_name, Employee manager) {
+        this.emp_no = emp_no;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.title = title;
+        this.salary = salary;
+        this.dept_name = dept_name;
+        this.manager = manager;
+    }
+
+    public int getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(int emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Department getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(Department dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "emp_no=" + emp_no +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", title='" + title + '\'' +
+                ", salary=" + salary +
+                ", dept_name='" + dept_name + '\'' +
+                ", manager='" + manager + '\'' +
+                '}';
+    }
 }
